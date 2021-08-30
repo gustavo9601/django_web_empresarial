461dda1ccf41a20221499195ec53c48fc3ac5887
gustavo9601 <tavo9601@gmail.com>
2021-08-29 22:58:10 -0500

    add restrictions to users in dashboard admin

--- webempresa/social/admin.py
+++ webempresa/social/admin.py
@@ -6,5 +6,13 @@ from .models import Link
 class LinkAdmin(admin.ModelAdmin):
     readonly_fields = ('created_at', 'updated_at')
 
+    # Permite añadir restricciones para campos en el damin
+    def get_readonly_fields(self, request, obj=None):
+        if request.user.groups.filter(name='personal').exists():
+            return ('created_at', 'updated_at', 'key', 'name')
+        else:
+            return ('created_at', 'updated_at')
+
+
 # Registrando
 admin.site.register(Link, LinkAdmin)
